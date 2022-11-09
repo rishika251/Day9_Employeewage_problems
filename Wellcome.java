c8ad8fac5819415713148031bbd18a757f10c676
rishika251 <rishikasadineni@gmail.com>
2022-11-09 19:49:39 +0530

    Added:Wellcome

---
+++ Wellcome.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Day9employeewage;
+
+public class Wellcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Wellcome to employee wage problems");
+
+	}
+
+}
