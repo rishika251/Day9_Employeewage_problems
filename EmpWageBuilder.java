4f734b4f62449378082656dda099680d2d8b2dcb
rishika251 <rishikasadineni@gmail.com>
2022-11-09 20:06:46 +0530

    Added: check employee wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,22 +1,32 @@
 package com.bridgelabz.Day9employeewage;
 
 public class EmpWageBuilder {
-	 static int FULL_TIME = 1;
+	static int FULL_TIME = 1;
+    static int PART_TIME = 2;
+    static int empPerHR_wage = 20;
 
-	    public static void main(String[] args) {
-	        //check emp present or not
-	        checkEmpPresent();
+    //
+    static int empHrs = 3;
+    public static void main(String[] args) {
+        getEmpDailyWage();
+    }
 
+    static double getEmpDailyWage() {
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck1 = Math.floor(Math.random() * 10) % 3;
+        if (empCheck1 == FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+         if (empCheck1 == PART_TIME)
+            empHrs = 4;
+        double empWage = empHrs * empPerHR_wage;
+        System.out.println("Employee Daily wage is " +empWage);
 
-	    }
-
-	    static double checkEmpPresent() {
-	        double empCheck = Math.floor(Math.random() * 10) % 2;
-	        if (empCheck == FULL_TIME)
-	            System.out.println("Employee is Present");
-	        else
-	            System.out.println("Employee is Present");
-
-	        return empCheck;
-	    }
+        return empWage;
+    }
 }
+
+ 
+
+		 
