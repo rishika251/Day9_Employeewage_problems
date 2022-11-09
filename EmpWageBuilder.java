63d308a295d2d4cd068c138f87865a6d16a2b017
rishika251 <rishikasadineni@gmail.com>
2022-11-09 20:16:49 +0530

    Added:Using switch case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,32 +1,42 @@
 package com.bridgelabz.Day9employeewage;
 
 public class EmpWageBuilder {
-	static int FULL_TIME = 1;
-    static int PART_TIME = 2;
+	static int FULL_TIME1 = 1;
+    static int PART_TIME1 = 2;
+    public static final int FULL_TIME = 1;
+    public static final int PART_TIME = 2;
     static int empPerHR_wage = 20;
 
     //
-    static int empHrs = 3;
+    static int empHrs = 0;
     public static void main(String[] args) {
         getEmpDailyWage();
     }
 
     static double getEmpDailyWage() {
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        double empCheck1 = Math.floor(Math.random() * 10) % 3;
-        if (empCheck1 == FULL_TIME)
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == FULL_TIME1)
             empHrs = 8;
-        else
-            empHrs = 0;
-         if (empCheck1 == PART_TIME)
+        else if (empCheck == PART_TIME)
             empHrs = 4;
+        switch ((int) empCheck) {
+            case FULL_TIME:
+                empHrs = 8;
+                break;
+            case PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+        }
         double empWage = empHrs * empPerHR_wage;
         System.out.println("Employee Daily wage is " +empWage);
 
         return empWage;
     }
+
 }
 
- 
+    
 
 		 
