18a4da0a7f80902015db979bcb6db46624891f55
rishika251 <rishikasadineni@gmail.com>
2022-11-09 20:19:42 +0530

    Added: calculate monthly wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,24 +1,24 @@
 package com.bridgelabz.Day9employeewage;
 
 public class EmpWageBuilder {
-	static int FULL_TIME1 = 1;
-    static int PART_TIME1 = 2;
-    public static final int FULL_TIME = 1;
+	public static final int FULL_TIME = 1;
     public static final int PART_TIME = 2;
     static int empPerHR_wage = 20;
 
     //
     static int empHrs = 0;
+    static int num_of_Working_days = 20;
+    static double empWage = 0;
+    static double totalEmpWage = 0;
+
     public static void main(String[] args) {
         getEmpDailyWage();
+        getEmpMonthWage();
+
     }
 
     static double getEmpDailyWage() {
         double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == FULL_TIME1)
-            empHrs = 8;
-        else if (empCheck == PART_TIME)
-            empHrs = 4;
         switch ((int) empCheck) {
             case FULL_TIME:
                 empHrs = 8;
@@ -32,11 +32,26 @@ public class EmpWageBuilder {
         double empWage = empHrs * empPerHR_wage;
         System.out.println("Employee Daily wage is " +empWage);
 
-        return empWage;
+        return empWage; }
+    static void getEmpMonthWage() {
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            switch (empCheck) {
+                case FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * empPerHR_wage;
+            totalEmpWage =empWage * num_of_Working_days;
+            System.out.println("Employee Daily wage is " + empWage+ " Rs");
+            System.out.println("Total Monthly Emp wage:- " +totalEmpWage+ " Rs");
+
     }
-
-}
-
+    }
     
 
 		 
